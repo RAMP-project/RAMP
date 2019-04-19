3a22d3cbc3ccfd5fb22014b7625ef71fd0102d2d
FLomb <francesco.lombardi@polimi.it>
2019-04-19 15:47:21 +0200

    v.02-pre

---
+++ RAMP_v02-pre/RAMP_run.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 14:35:00 2019
+This is the code for the open-source stochastic model for the generation of 
+multi-energy load profiles in off-grid areas, called RAMP, v.0.2-pre.
+
+@authors:
+- Francesco Lombardi, Politecnico di Milano
+- Sergio Balderrama, Université de Liège
+- Sylvain Quoilin, KU Leuven
+- Emanuela Colombo, Politecnico di Milano
+
+Copyright 2019 RAMP, contributors listed above.
+Licensed under the European Union Public Licence (EUPL), Version 1.1;
+you may not use this file except in compliance with the License.
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations
+under the License.
+"""
+
+#%% Import required libraries
+
+from post_process import*
