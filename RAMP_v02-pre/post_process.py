5e9434383c2a24ccf14f1dabdf6fccfda1e72277
FLomb <francesco.lombardi@polimi.it>
2019-04-30 15:29:57 +0200

    Updated plots
    Plots changed to produce by default the series of profiles

--- RAMP_v02-pre/post_process.py
+++ RAMP_v02-pre/post_process.py
@@ -24,21 +24,35 @@ Profile_series = np.array([])
 for iii in Profile:
     Profile_series = np.append(Profile_series,iii)
 
+#==============================================================================
+# x = np.arange(0,1440,5)
+# plt.figure(figsize=(10,5))
+# for n in Profile:
+#     plt.plot(np.arange(1440),n,'#b0c4de')
+#     plt.xlabel('Time (hours)')
+#     plt.ylabel('Power (W)')
+#     plt.ylim(ymin=0)
+#     #plt.ylim(ymax=5000)
+#     plt.margins(x=0)
+#     plt.margins(y=0)
+# plt.plot(np.arange(1440),Profile_avg,'#4169e1')
+# plt.xticks([0,240,480,(60*12),(60*16),(60*20),(60*24)],[0,4,8,12,16,20,24])
+# #plt.savefig('profiles.eps', format='eps', dpi=1000)
+# plt.show()
+#==============================================================================
+
 x = np.arange(0,1440,5)
 plt.figure(figsize=(10,5))
-for n in Profile:
-    plt.plot(np.arange(1440),n,'#b0c4de')
-    plt.xlabel('Time (hours)')
-    plt.ylabel('Power (W)')
-    plt.ylim(ymin=0)
-    #plt.ylim(ymax=5000)
-    plt.margins(x=0)
-    plt.margins(y=0)
-plt.plot(np.arange(1440),Profile_avg,'#4169e1')
-plt.xticks([0,240,480,(60*12),(60*16),(60*20),(60*24)],[0,4,8,12,16,20,24])
+plt.plot(np.arange(len(Profile_series)),Profile_series,'#4169e1')
+#plt.xlabel('Time (hours)')
+plt.ylabel('Power (W)')
+plt.ylim(ymin=0)
+#plt.ylim(ymax=5000)
+plt.margins(x=0)
+plt.margins(y=0)
+#plt.xticks([0,240,480,(60*12),(60*16),(60*20),(60*24)],[0,4,8,12,16,20,24])
 #plt.savefig('profiles.eps', format='eps', dpi=1000)
 plt.show()
-
 
 #%% Export individual profiles
 '''
