e3e893f1395296bb44648ae2ee11892cd1d0f68a
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2022-05-12 00:30:18 +0200

    Enable running the code from old and new input files

--- ramp/core/initialise.py
+++ ramp/core/initialise.py
@@ -4,6 +4,7 @@
 
 import numpy as np 
 import importlib
+from ramp.core.core import UseCase
 
 
 def yearly_pattern():
@@ -18,32 +19,38 @@ def yearly_pattern():
     return(Year_behaviour)
 
 
-def user_defined_inputs(j):
+def user_defined_inputs(j=None, fname=None):
     '''
     Imports an input file and returns a processed User_list
     '''
+    # Back compatibility with old code
+    if j is not None:
+        file_module = importlib.import_module(f'input_files.input_file_{j}')
+        User_list = file_module.User_list
 
-    file_module = importlib.import_module(f'input_files.input_file_{j}')
-    
-    User_list = file_module.User_list
+    if fname is not None:
+        usecase = UseCase()
+        usecase.load(fname)
+        User_list = usecase.users
 
     return(User_list)
 
 
-def Initialise_model():
+def Initialise_model(num_profiles):
     '''
     The model is ready to be initialised
     '''
-    num_profiles = int(input("please indicate the number of profiles to be generated: ")) #asks the user how many profiles (i.e. code runs) he wants
+
+    if num_profiles is None:
+        num_profiles = int(input("please indicate the number of profiles to be generated: ")) #asks the user how many profiles (i.e. code runs) he wants
     print('Please wait...') 
     Profile = [] #creates an empty list to store the results of each code run, i.e. each stochastically generated profile
     
     return (Profile, num_profiles)
     
-def Initialise_inputs(j):
+def Initialise_inputs(j=None, fname=None):
     Year_behaviour = yearly_pattern()
-    user_defined_inputs(j)
-    user_list = user_defined_inputs(j)
+    user_list = user_defined_inputs(j, fname)
     
     # Calibration parameters
     '''
