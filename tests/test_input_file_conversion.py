636724c58a2daa96c91335c211a19cdae6ea3c69
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2022-06-13 22:20:03 +0200

    Add test to define minimal number of windows

--- tests/test_input_file_conversion.py
+++ tests/test_input_file_conversion.py
@@ -103,3 +103,65 @@ def test_define_appliance_duty_cycle_directly_equivalent_to_use_specific_cycle_m
     appliance2 = user.add_appliance(**params)
 
     assert appliance1 == appliance2
+
+
+def test_provide_only_one_appliance_window_when_declaring_two():
+    user = User("test user", 1)
+
+    params = dict(number=1, power=200, num_windows=2, func_time=0)
+    win_start = 390
+    win_stop = 540
+    with pytest.raises(ValueError):
+        appliance1 = user.add_appliance(**params)
+        appliance1.windows(window_1=[win_start, win_stop])
+    with pytest.raises(ValueError):
+        params.update({"window_1": np.array([win_start, win_stop])})
+        user.add_appliance(**params)
+
+
+def test_provide_no_appliance_window_when_declaring_one():
+    user = User("test user", 1)
+
+    params = dict(number=1, power=200, num_windows=1, func_time=0)
+    with pytest.warns(UserWarning):
+        appliance1 = user.add_appliance(**params)
+        appliance1.windows()
+    with pytest.warns(UserWarning):
+        params.update({"window_1": None})
+        user.add_appliance(**params)
+
+
+def test_A():
+    user = User("test user", 1)
+
+    old_params = dict(n=1, P=200, w=1, t=0)
+    win_start = 390
+    win_stop = 540
+    appliance1 = user.Appliance(user, **old_params)
+    appliance1.windows(window_1=[win_start, win_stop])
+
+    params = dict(number=1, power=200, num_windows=1, func_time=0, window_1=np.array([win_start, win_stop]))
+    appliance2 = user.add_appliance(**params)
+
+    assert appliance1 == appliance2
+
+def test_B():
+    user = User("test user", 1)
+
+    params = dict(
+        number=1,
+        power=200,
+        num_windows=1,
+        func_time=0,
+        window_1=[390, 540],
+        fixed_cycle=1,
+    )
+
+    appliance1 = user.add_appliance(**params)
+    cycle_params = {"p_11": 20, "t_11": 10, "cw11": np.array([400, 500])}
+    appliance1.specific_cycle_1(**cycle_params)
+
+    params.update(cycle_params)
+    appliance2 = user.add_appliance(**params)
+
+    assert appliance1 == appliance2
