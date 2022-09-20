5c3c99ffe2d21b659db29248eb1a0f2c1bde118d
F.L <f.lombardi@outlook.com>
2022-09-20 16:22:28 +0200

    Fix typo in example input file

--- ramp/input_files/input_file_1.py
+++ ramp/input_files/input_file_1.py
@@ -51,7 +51,7 @@ Ch_indoor_bulb.windows([1200,1440],[0,0],0.1)
 Ch_outdoor_bulb = Church.Appliance(Church,7,26,1,240,0.2,60, 'yes', flat = 'yes')
 Ch_outdoor_bulb.windows([1200,1440],[0,0],0.1)
 
-Ch_speaker = Church.Appliance(Church,1,100,1,240,0.2,60)
+Ch_speaker = Church.Appliance(Church,1,100,1,150,0.2,60)
 Ch_speaker.windows([1200,1350],[0,0],0.1)
 
 #Public lighting
